803e0976c006fb5b6802fadc74eca35cb691c7e2
gangstaqwe <igorcr228@gmail.com>
2020-08-04 16:48:05 +0300

    List
    some problems without list Information

---
+++ FlagmanShop_Test/TestSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlagmanShop_Test
+{
+    public class TestSettings
+    {
+        public static string HostPrefix = "https://www.flagman.kiev.ua/";
+        public static string UrlToOrderProduct = "http://altron.ua/buying.php?p_id=17&lang=ru-RU";
+    }
+
+
+}
